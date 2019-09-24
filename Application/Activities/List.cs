df3269829c2373d6cfa9e0438e63d073f018a10f
AlanTw <Alanwisny@hotmail.com>
2019-09-24 19:30:53 +0200

    Basic React Components

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -17,10 +17,10 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
-            public Handler(DataContext context, ILogger<List> logger)
+            //private readonly ILogger<List> _logger;
+            public Handler(DataContext context)
             {
-                _logger = logger;
+                //_logger = logger;
                 _context = context;
             }
 
