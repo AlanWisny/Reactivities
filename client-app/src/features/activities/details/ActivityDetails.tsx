659c2360188d4eecf6b29bd119984db81275ef83
AlanTw <Alanwisny@hotmail.com>
2019-09-26 17:16:59 +0200

    End of Section 5

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Image, Icon, Button } from "semantic-ui-react";
+import { Card, Image, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
@@ -35,7 +35,7 @@ const ActivityDetails: React.FC<IProps> = ({
             color="blue"
             content="Edit"
           />
-          <Button onClick={() => setSelectedActivity(null)} basic color="grey" content="Cancel" />
+          <Button onClick={() => setSelectedActivity(null)} basic color="purple" content="Cancel" />
         </Button.Group>
       </Card.Content>
     </Card>
